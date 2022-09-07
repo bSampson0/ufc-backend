4168d472b3928ee97ba8d5d2a2795e597574bf5c
besampson0 <bnicerrr@gmail.com>
2022-09-07 09:06:42 -0500

    updates

--- src/main/java/com/app/ufc/repository/FighterRepository.java
+++
@@ -1,7 +0,0 @@
-package com.app.ufc.repository;
-
-import com.app.ufc.fighter.Fighter;
-import org.springframework.data.repository.CrudRepository;
-
-public interface FighterRepository extends CrudRepository<Fighter, Long> {
-}
