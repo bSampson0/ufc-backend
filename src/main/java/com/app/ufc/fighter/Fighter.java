f86bbbb4b33ac0be6d5bcb2cc8ef2185367be145
besampson0 <bnicerrr@gmail.com>
2022-09-07 14:44:40 -0500

    add fighter to table

--- src/main/java/com/app/ufc/fighter/Fighter.java
+++ src/main/java/com/app/ufc/fighter/Fighter.java
@@ -1,6 +1,29 @@
 package com.app.ufc.fighter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table
 public class Fighter {
+  @Id
+  @SequenceGenerator(
+    name = "fighter_sequence",
+    sequenceName = "fighter_sequence",
+    allocationSize = 1
+  )
+  @GeneratedValue(
+    strategy = GenerationType.SEQUENCE,
+    generator = "fighter_sequence"
+  )
+  private Long id;
+  private String name;
+  private String weight_class;
+
   public Fighter() {
   }
   public Fighter(Long id, String name, String weight_class) {
@@ -8,21 +31,18 @@ public class Fighter {
     this.name = name;
     this.weight_class = weight_class;
   }
-  private Long id;
   public Long getId() {
     return id;
   }
   public void setId(Long id) {
     this.id = id;
   }
-  private String name;
   public String getName() {
     return name;
   }
   public void setName(String name) {
     this.name = name;
   }
-  private String weight_class;
   public Fighter(String name, String weight_class) {
     this.name = name;
     this.weight_class = weight_class;
